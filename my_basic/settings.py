cbc3600514bc67c1b17f9012b5e8a46437412eb7
ka-pa-ra <capriolo63@gmail.com>
2020-12-29 21:18:33 +0000

    added checkout form data caching in payment intent

--- my_basic/settings.py
+++ my_basic/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
 
-    #others
+    # others
     'crispy_forms',
 ]
 
@@ -174,3 +174,4 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
